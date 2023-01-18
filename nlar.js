078a6f99c9729289570b70402e10587716886b97
Sơn <41245943+Meliodaspro@users.noreply.github.com>
2023-01-18 23:35:02 +0700

    ok pushh

--- nlar.js
+++ nlar.js
@@ -40,3 +40,20 @@ app.controller('myController', function($scope) {
 		$scope.gmail= !$scope.gmail;
 	};
 });
+/*--------------------------------check connected------------------------*/
+let alerts = document.querySelectorAll('.alert');
+alerts.forEach(item=>{
+    item.addEventListener('click', function(event){
+        if(event.target.classList.contains('close')){
+            item.style.display = 'none';
+        }
+    })
+})
+window.addEventListener('offline', function(){
+    document.getElementById('success').style.display = 'none';
+    document.getElementById('error').style.display = 'grid';
+})
+window.addEventListener('online', function(){
+    document.getElementById('error').style.display = 'none';
+    document.getElementById('success').style.display ='grid';
+});
